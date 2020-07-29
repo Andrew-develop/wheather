552259c61275cb707e732f8f8ec76e0ee97ad1e0
Andrew Frolov <adm.kolchak.221001@gmail.com>
2020-07-29 12:45:47 +0700

    fixed bugs

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UISearchBarDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var picker: UIPickerView!
@@ -21,49 +21,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var cityResponse = ""
     var temperature = 0
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        city = pickerData[row]
-    }
-    
-    var pickerData: [String] = [String]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.picker.delegate = self
-        self.picker.dataSource = self
-        
-        pickerData = ["London", "Moscow", "Tomsk", "Sochi", "Madrid"]
-    }
-    
+    let pickerData = ["London", "Moscow", "Tomsk", "Sochi", "Madrid"]
     
     @IBAction func okButton(_ sender: UIButton) {
         AF.request("http://api.weatherstack.com/current?access_key=45b333bfd48cc2c5a61bfef1be84f46a&query=\(city)").responseJSON { responseJSON in
-            switch responseJSON.result {
-            case .success(let value):
-                print("Success got the weather data")
-                let weatherJSON : JSON = JSON(responseJSON.value)
-                self.updateWeatherData(json: weatherJSON)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        AF.request("http://api.weatherstack.com/current?access_key=45b333bfd48cc2c5a61bfef1be84f46a&query=\(searchBar.text)").responseJSON { responseJSON in
             switch responseJSON.result {
             case .success(let value):
                 print("Success got the weather data")
@@ -89,6 +50,40 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func updateUIWithWeatherData() {
         cityLabel.text = cityResponse
         temperatureLabel.text = String(temperature)
+    }
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        city = pickerData[row]
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        AF.request("http://api.weatherstack.com/current?access_key=45b333bfd48cc2c5a61bfef1be84f46a&query=\(searchBar.text!)").responseJSON { responseJSON in
+            switch responseJSON.result {
+            case .success(let value):
+                print("Success got the weather data")
+                let weatherJSON : JSON = JSON(responseJSON.value)
+                self.updateWeatherData(json: weatherJSON)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
